73a796c8bd9d54f575cb34f0bf06f22255950961
Yasitha Sanjula Herath <yasithasanjula2018@gmail.com>
2023-11-15 19:25:54 +0530

    11

---
+++ Navbar1/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Yasitha Sanjula Herath</h1>
+    </>
+  )
+}
+
+export default App
